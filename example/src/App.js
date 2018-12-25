affa9a7d33f26c37b01d18208fa784921f3091d7
Wei-Ming Chen <barry800414@gmail.com>
2018-12-25 16:44:44 +0800

    update: just update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'hh-slate-editor'
+import SlateEditor from 'hh-slate-editor'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <h1>Slate Editor Demo</h1>
+        <SlateEditor text='Modern React component module' />
       </div>
     )
   }
