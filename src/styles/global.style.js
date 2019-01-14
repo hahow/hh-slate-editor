caa9fafaf6ea94bff4496cfd92da54d01ae2a6ef
Wei-Ming Chen <barry800414@gmail.com>
2019-01-14 12:49:38 +0800

    refactor: remove unused typography and form styles, remove injectGlobal

--- src/styles/global.style.js
+++
@@ -1,9 +0,0 @@
-import { injectGlobal } from 'styled-components';
-import { Typography } from './typography.style';
-import { Form } from './form.style';
-
-/* eslint no-unused-expressions: 0 */
-injectGlobal`
-  ${Typography}
-  ${Form}
-`;
